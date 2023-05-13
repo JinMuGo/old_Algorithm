234e0b7dcc17ca62048778990f4c04335ffc6736
jgo-42 <jingmuio@gmail.com>
2023-05-13 11:59:56 +0900

    :white_check_mark: BOJ: 별 찍기 - 17 / Bronze 3

---
+++ BaekJoon/c++/plzrun/입출력 ~ 이런저런문제/10992.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   10992.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/13 11:33:04 by jgo               #+#    #+#             */
+/*   Updated: 2023/05/13 11:59:15 by jgo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+static void prtStar(const int N, int L) {
+	if (L == N + 1)
+		return;
+	for (int i = 0; i < N - L; ++i)
+		std::cout << " ";
+	for (int i = 0; i < L * 2 - 1; ++i) {
+		if (i != 0 && i != L * 2 - 2 && L != N)
+			std::cout << " ";
+		else
+			std::cout << "*";
+	}
+	std::cout << "\n";
+	prtStar(N, L + 1);
+}
+
+int main(void) {
+	int N;
+
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	std::cin >> N;
+
+	prtStar(N, 1);
+
+	return (0);
+}
